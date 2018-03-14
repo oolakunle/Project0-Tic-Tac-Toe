ea7cd978621773e605ce98d755523af8d1acdb8c
Abdulmajeed Yusuf <oolakunle@ymail.com>
2018-03-14 14:25:08 +1100

    initial complete project

---
+++ README.md
@@ -0,0 +1,47 @@
+# Project 0
+
+Some **regular** text
+
+Some *more* regular _text_
+
+This is a ***new*** paragraph.
+
+## Lists
+
+* list item
+* second item
+  * sub item
+  * sub item two
+
+### Marx Brothers
+1. Groucho
+2. Harpo
+1. Chico
+
+![Bill Murray](http://fillmurray.com/50/70)
+
+http://twitter.com
+
+[Twitter](http:twitter.com)
+
+To quote someone famous:
+
+>Famous inspirational quote
+
+You should use the `<section>` tag.
+
+There are too many variables called `scored` in this project.
+
+## Github Flavoured Markdown
+
+### Function expression example
+
+```
+const sayHello = function () {
+  console.log('Hello');
+}
+```
+
+@coldhead                        >tag people
+
+~~this is struck out~~ updated docs.
---
+++ js/main.js
@@ -0,0 +1,102 @@
+let player = 1;
+
+const clickButton = function (button) {
+  if (player === 1) {
+      // Assign the value of X to the chosen block on the grid
+      document.getElementById(button).value = "X";
+      // Stop the button from changing value
+      document.getElementById(button).disabled = "disabled";
+      // Alternate between Player 1 and 2.
+      player += 1;
+      winner();
+  } else {
+    document.getElementById(button).value = "O"
+    document.getElementById(button).disabled = "disabled";
+    player -= 1;
+    winner();
+  }
+
+}
+
+
+const winner = function () {
+  if (document.getElementById("button1").value === "X" &&
+      document.getElementById("button2").value === "X" &&
+      document.getElementById("button3").value === "X" ||
+      document.getElementById("button4").value === "X" &&
+      document.getElementById("button5").value === "X" &&
+      document.getElementById("button6").value === "X" ||
+      document.getElementById("button7").value === "X" &&
+      document.getElementById("button8").value === "X" &&
+      document.getElementById("button9").value === "X" ||
+      document.getElementById("button1").value === "X" &&
+      document.getElementById("button4").value === "X" &&
+      document.getElementById("button7").value === "X" ||
+      document.getElementById("button2").value === "X" &&
+      document.getElementById("button5").value === "X" &&
+      document.getElementById("button8").value === "X" ||
+      document.getElementById("button3").value === "X" &&
+      document.getElementById("button6").value === "X" &&
+      document.getElementById("button9").value === "X" ||
+      document.getElementById("button1").value === "X" &&
+      document.getElementById("button5").value === "X" &&
+      document.getElementById("button9").value === "X" ||
+      document.getElementById("button3").value === "X" &&
+      document.getElementById("button5").value === "X" &&
+      document.getElementById("button7").value === "X"
+    ) {
+        alert("Player 1 won!!!");
+        reset();
+      } else if (document.getElementById("button1").value === "O" &&
+          document.getElementById("button2").value === "O" &&
+          document.getElementById("button3").value === "O" ||
+          document.getElementById("button4").value === "O" &&
+          document.getElementById("button5").value === "O" &&
+          document.getElementById("button6").value === "O" ||
+          document.getElementById("button7").value === "O" &&
+          document.getElementById("button8").value === "O" &&
+          document.getElementById("button9").value === "O" ||
+          document.getElementById("button1").value === "O" &&
+          document.getElementById("button4").value === "O" &&
+          document.getElementById("button7").value === "O" ||
+          document.getElementById("button2").value === "O" &&
+          document.getElementById("button5").value === "O" &&
+          document.getElementById("button8").value === "O" ||
+          document.getElementById("button3").value === "O" &&
+          document.getElementById("button6").value === "O" &&
+          document.getElementById("button9").value === "O" ||
+          document.getElementById("button1").value === "O" &&
+          document.getElementById("button5").value === "O" &&
+          document.getElementById("button9").value === "O" ||
+          document.getElementById("button3").value === "O" &&
+          document.getElementById("button5").value === "O" &&
+          document.getElementById("button7").value === "O"
+        ) {
+            alert("Player 2 won!!!");
+            reset();
+          }
+}
+
+const reset = function () {
+  document.getElementById("button1").value = "";
+  document.getElementById("button2").value = "";
+  document.getElementById("button3").value = "";
+  document.getElementById("button4").value = "";
+  document.getElementById("button5").value = "";
+  document.getElementById("button6").value = "";
+  document.getElementById("button7").value = "";
+  document.getElementById("button8").value = "";
+  document.getElementById("button9").value = "";
+  document.getElementById("button1").disabled = "";
+  document.getElementById("button2").disabled = "";
+  document.getElementById("button3").disabled = "";
+  document.getElementById("button4").disabled = "";
+  document.getElementById("button5").disabled = "";
+  document.getElementById("button6").disabled = "";
+  document.getElementById("button7").disabled = "";
+  document.getElementById("button8").disabled = "";
+  document.getElementById("button9").disabled = "";
+  console.log("Hello");
+}
+
+// $('#resetButton').on('click', reset)
